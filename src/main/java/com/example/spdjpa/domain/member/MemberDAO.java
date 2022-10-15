e4aaf2289b4bfc0ee7336bf0111f563305356360
Jwhyee <kawnsdud@naver.com>
2022-10-15 16:53:52 +0900

    feat #1 : Implement find member DAO

--- src/main/java/com/example/spdjpa/domain/member/MemberDAO.java
+++ src/main/java/com/example/spdjpa/domain/member/MemberDAO.java
@@ -1,2 +1,27 @@
-package com.example.spdjpa.domain.member;public class MemberDAO {
+package com.example.spdjpa.domain.member;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class MemberDAO {
+    Connection con;
+    Statement stmt;
+    ResultSet rs;
+
+    public Member find(String memberId) throws SQLException {
+        String sql = "SELECT MEMBER_ID, NAME FROM MEMBER M WHERE MEMBER_ID = ?";
+        stmt = con.createStatement();
+        rs = stmt.executeQuery(sql);
+
+        String findMemberId = rs.getString("MEMBER_ID");
+        String findMemberName = rs.getString("NAME");
+
+        Member member = new Member();
+        member.setMemberId(findMemberId);
+        member.setName(findMemberName);
+
+        return member;
+    }
 }
