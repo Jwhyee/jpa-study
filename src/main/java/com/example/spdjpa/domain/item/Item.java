c643310269295b21e7c17ca0e2a610cb0eef067a
Jwhyee <kawnsdud@naver.com>
2022-10-15 18:18:19 +0900

    add #1 : Add Item class for Object Modeling example

---
+++ src/main/java/com/example/spdjpa/domain/item/Item.java
@@ -0,0 +1,21 @@
+package com.example.spdjpa.domain.item;
+
+public class Item {
+    Long id;
+    String name;
+    int price;
+}
+
+class Album extends Item {
+    String artist;
+}
+
+class Movie extends Item {
+    String director;
+    String actor;
+}
+
+class Book extends Item {
+    String author;
+    String isbn;
+}
