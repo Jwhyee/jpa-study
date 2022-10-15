2a12a980454ef9c39c54950af0791b3744f9bf7e
Jwhyee <kawnsdud@naver.com>
2022-10-15 16:25:38 +0900

    setting : Project Setting

---
+++ src/main/java/com/example/spdjpa/SpdjpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.spdjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpdjpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpdjpaApplication.class, args);
+	}
+
+}
