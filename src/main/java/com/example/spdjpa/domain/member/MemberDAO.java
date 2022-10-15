0df95ea93b3569d563594e93de527bd25fa206bf
Jwhyee <kawnsdud@naver.com>
2022-10-15 17:06:48 +0900

    refactor #1 : Refactor stmt to pstmt naming

--- src/main/java/com/example/spdjpa/domain/member/MemberDAO.java
+++ src/main/java/com/example/spdjpa/domain/member/MemberDAO.java
@@ -4,13 +4,13 @@ import java.sql.*;
 
 public class MemberDAO {
     Connection con;
-    PreparedStatement stmt;
+    PreparedStatement pstmt;
     ResultSet rs;
 
     public Member find(String memberId) throws SQLException {
         String sql = "SELECT MEMBER_ID, NAME FROM MEMBER M WHERE MEMBER_ID = ?";
-        stmt.setString(1, memberId);
-        rs = stmt.executeQuery(sql);
+        pstmt.setString(1, memberId);
+        rs = pstmt.executeQuery(sql);
 
         String findMemberId = rs.getString("MEMBER_ID");
         String findMemberName = rs.getString("NAME");
@@ -24,9 +24,9 @@ public class MemberDAO {
 
     public void save(Member member) throws SQLException {
         String sql = "INSERT INTO MEMBER(MEMBER_ID, NAME) VALUES(?, ?)";
-        stmt.setString(1, member.getMemberId());
-        stmt.setString(2, member.getName());
+        pstmt.setString(1, member.getMemberId());
+        pstmt.setString(2, member.getName());
 
-        stmt.executeUpdate(sql);
+        pstmt.executeUpdate(sql);
     }
 }
